c3ff1e5e0eb4acbfaa9a9a0ee2ad6a11a9292cf2
omkanekar28 <omkanekar28@gmail.com>
2024-12-28 17:35:13 +0530

    Disabled debug mode

--- src/app.py
+++ src/app.py
@@ -70,4 +70,4 @@ def find_your_agent() -> Union[str, Response]:
             return jsonify({"error": str(e)}), 500
         
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0')
